c6804bdecd9edc75b681f87038011431ed8030ad
Remant <remantmahato9798@gmail.com>
2022-04-05 23:12:56 +0530

    Added : UC1-ValidFirstName

---
+++ UserRegistrationTesting/UserRegistrationTesting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UserRegistrationMSTesting;
+
+namespace UserRegistrationTesting
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Validation userRegistration = new Validation();
+            int options = 0;
+            do
+            {
+                Console.WriteLine("Choose 1: First name ");
+
+                try
+                {
+                    options = int.Parse(Console.ReadLine());
+                    switch (options)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter your First Name: ");
+                            string input = Console.ReadLine();
+                            userRegistration.ValidFirstName(input);
+                            break;
+                        default:
+                            Console.WriteLine("Choose From the Options given above");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Choose a valid option");
+                }
+            } while (options != 0);
+        }
+    }
+}
